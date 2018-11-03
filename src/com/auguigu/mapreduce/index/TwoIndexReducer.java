5587128c17d0f2af424e0fdf3c32b74c9c8ba11b
huangyu1996 <sdu_huangyu@foxmail.com>
2018-11-03 19:50:19 +0800

    修改

--- src/com/auguigu/mapreduce/index/TwoIndexReducer.java
+++ src/com/auguigu/mapreduce/index/TwoIndexReducer.java
@@ -1,4 +1,5 @@
 package com.auguigu.mapreduce.index;
+
 import org.apache.hadoop.mapreduce.Reducer;
 
 import org.apache.hadoop.io.IntWritable;
@@ -6,6 +7,18 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
-public class TwoIndexReducer extends Reducer<Text,Text,>
-{
+
+import java.io.IOException;
+
+public class TwoIndexReducer extends Reducer<Text, Text, Text, Text> {
+    Text v = new Text();
+
+    @Override
+    protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        StringBuilder sb = new StringBuilder();
+        for (Text text : values) sb.append(text.toString() + "\t");
+        //设置输出value
+        v.set(sb.toString());
+        context.write(key, v);
+    }
 }
